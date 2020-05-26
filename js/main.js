500bd38b439c9667c08c4dc7b961db554af5b99e
Leo Kosola <leokosola@gmail.com>
2020-05-26 10:12:07 +0300

    kuvia, linkkejä

---
+++ js/main.js
@@ -0,0 +1,36 @@
+console.log(navigator.onLine);
+
+console.log(navigator.userAgent);
+
+console.log(new Date());
+
+console.log(history.length);
+
+console.log(document.referrer);
+
+console.log(navigator.appName);
+console.log(navigator.product);
+console.log(navigator.platform);
+
+console.log(screen.width);
+console.log(screen.height);
+
+console.log(location.port);
+
+document.body.addEventListener(onclick, function() {
+  console.log(innerWidth);
+});
+
+console.log(scrollX, scrollY);
+
+function scroll () {
+  console.log("arse");
+
+  if(pageYOffset > 100) {
+    console.log("perse");
+  }
+
+}
+
+scroll();
+
